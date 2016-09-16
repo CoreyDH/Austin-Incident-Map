f90d4d26c2b8b3595e4c76d52f2c053d26c5a6bc
Gael Arrambide <gael_arrambide@hotmail.com>
2016-09-15 20:25:05 -0500

    adding strict bounds

--- assets/js/map.js
+++ assets/js/map.js
@@ -11,11 +11,36 @@ function initMap() {
     var geocode= new google.maps.Geocoder();
    	var minZoomLevel = 10;
    	var map = new google.maps.Map(document.getElementById('map'), {
-    	center: {lat: 30.29, lng: -97.666667},
+    	center: new google.maps.LatLng(30.29,  -97.666667),
     	zoom: 11,
     	mapTypeControl: false,
     	streetViewControl: false,
   	});
+  	var strictBounds = new google.maps.LatLngBounds(
+     new google.maps.LatLng(28.70, -127.50), 
+     new google.maps.LatLng(48.85, -55.90)
+   );
+
+  	 google.maps.event.addListener(map, 'dragend', function() {
+     if (strictBounds.contains(map.getCenter())) return;
+
+     // We're out of bounds - Move the map back within the bounds
+     var c = map.getCenter(),
+         x = c.lng(),
+         y = c.lat(),
+         maxX = strictBounds.getNorthEast().lng(),
+         maxY = strictBounds.getNorthEast().lat(),
+         minX = strictBounds.getSouthWest().lng(),
+         minY = strictBounds.getSouthWest().lat();
+
+     if (x < minX) x = minX;
+     if (x > maxX) x = maxX;
+     if (y < minY) y = minY;
+     if (y > maxY) y = maxY;
+
+     map.setCenter(new google.maps.LatLng(y, x));
+   });
+
   	google.maps.event.addListener(map, 'zoom_changed', function() {
      if (map.getZoom()<minZoomLevel){ 
      	map.setZoom(11);
