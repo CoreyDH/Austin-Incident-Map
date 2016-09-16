bbc63f5240932c7590550d8b742c9f4e031a24a4
Gael Arrambide <gael_arrambide@hotmail.com>
2016-09-15 20:16:41 -0500

    map zoom out limit

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,67 +1,48 @@
 //function to display map
-
+function plotDataByKeyword(keyword, limit){
+	var arrayToPlot=getDataByKeyword(keyword, limit);
+	plotMarkers(arrayToPlot);
+}
+function plotDataByCategory(category, limit){
+	var arrayToPlot=getDataByCategory(category, limit);
+	plotMarkers(arrayToPlot);
+};
 function initMap() {
-	object={
-		incidents: [
-		{crime_type: 'Assault',
-		address: '7630 Wood Hollow',
-		date: 'September 13 2016',
-		incident_report_number: '3635567807867856',
-		time: '142',
-		},
-		{crime_type: 'Identity Crime',
-		address: '2525 West Anderson',
-		date: 'September 9 2016',
-		incident_report_number: '65786708908',
-		time: '1004',
-		},
-		{crime_type: 'Thief',
-		address: '108 Denson Dr ',
-		date: 'September 6 2016',
-		incident_report_number: '675698089858',
-		time: '20',
-		},
-		],
-	}
     var geocode= new google.maps.Geocoder();
-   	var incidentWindow = new google.maps.InfoWindow({
-        content: contentString
-    });
+   	var minZoomLevel = 10;
    	var map = new google.maps.Map(document.getElementById('map'), {
     	center: {lat: 30.29, lng: -97.666667},
     	zoom: 11,
     	mapTypeControl: false,
     	streetViewControl: false,
   	});
-
-  	for(i=0; i<object.incidents.length; i++){
-  		key=object.incidents;
-  		var time = convertTime(key[i].time);
-  		var contentString = '<div id="content">'+
-      	'<div id="siteNotice">'+
-      	'</div>'+
-      	'<div id="bodyContent">'+
-      	'<p><b>Incident: </b>'+key[i].crime_type+'</p>'+
-      	'<p><b>Address: </b>'+key[i].address+'</p>'+
-      	'<p><b>Date: </b>'+key[i].date+'</p>'+
-      	'<p><b>Report Number: </b>'+key[i].incident_report_number+'</p>'+
-      	'<p><b>Time: </b>'+time+'</p>'+
-      	'</div>'+
-      	'</div>';
-      	var incidentWindow = new google.maps.InfoWindow({
-        content: contentString
-    	});
-    	createMarkers(geocode, map, key[i].address, incidentWindow);
-	}	
+  	google.maps.event.addListener(map, 'zoom_changed', function() {
+     if (map.getZoom()<minZoomLevel){ 
+     	map.setZoom(11);
+     }
+   });
 };
 
-function createMarkers(geocoder, resultsMap, incidentAddress, incidentWindow){
+function createMarkers(geocoder, resultsMap, incidentAddress, incidentWindow, markerImage){
 		var address = incidentAddress;
+		var image;
+		if (markerImage=="Assault"){
+			image="assets/images/robbery.png";
+		} else if (markerImage=="Identity Crime"){
+			image="assets/images/pirates.png";
+		} else if(markerImage=="Thief"){
+			image="assets/images/theft.png";
+		} else if (markerImage=="Car Accident"){
+			image="assets/images/caraccident.png";
+		} else if(markerImage=="Drug Crime"){
+			image="assets/images/.png";
+		}
 		geocoder.geocode({'address': address}, function(results, status) {
 		if (status === 'OK') {
         	resultsMap.setCenter(results[0].geometry.location);
         	var marker= new google.maps.Marker({
           	map: resultsMap,
+          	icon: image,
           	position: results[0].geometry.location
         	});
         marker.addListener('click', function(){
@@ -69,6 +50,7 @@ function createMarkers(geocoder, resultsMap, incidentAddress, incidentWindow){
         })
       	};
       });
+	//to erase markers use marker.setMap(null);
 }
 
 function convertTime(time){
@@ -94,4 +76,22 @@ function convertTime(time){
     displayTime=hour+':'+minutes + amPm;
 		return displayTime
 }
- 	
+function plotMarkers(arrayToPlot){
+
+	var incidentWindow = new google.maps.InfoWindow({
+        content: contentString
+    });
+	for(i=0; i<object.incidents.length; i++){
+  		key=object.incidents;
+  		var time = convertTime(key[i].time);
+  		var contentString = '<div id="content">'+
+      	'<div id="bodyContent">'+'<p><b>Incident: </b>'+key[i].crime_type+'</p>'+
+      	'<p><b>Address: </b>'+key[i].address+'</p>'+'<p><b>Date: </b>'+key[i].date+'</p>'+
+      	'<p><b>Report Number: </b>'+key[i].incident_report_number+'</p>'+'<p><b>Time: </b>'+time+'</p>'+
+      	'</div>'+'</div>';
+      	var incidentWindow = new google.maps.InfoWindow({
+        content: contentString
+    	});
+    	createMarkers(geocode, map, key[i].address, incidentWindow, key[i].crime_type);
+	}
+};
