bd9242426b76bb52c71948266eba7e67f13882bd
Armin Gutzmer <armin.dean75@gmail.com>
2016-09-16 21:26:20 -0500

    data.js with - getDataByKeyword function - categories:  theft, accident, and crash

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,60 +1,115 @@
-//
-// global variables and functions
-//
-var apdDataBase;
+(function($) {
+  $(function() {
+  //
+  // global variables and functions
+  //
+  var apdDataBase;
 
-//
-// Function: Read in nad init the apd database
-//
-  function initDatabase () {
-   $.ajax({
-        url: "https://data.austintexas.gov/resource/rkrg-9tez.json",
-        async: false,
-        type: "GET",
-        data: {
-          // "$limit" : 10,
-          "$$app_token" : "jdmsP3hiF9ZYMZMlJTaiBOKly"
+  //
+  // Function: Read in nad init the apd database
+  //
+    function initDatabase(callback) {
+     $.ajax({
+          url: "https://data.austintexas.gov/resource/rkrg-9tez.json",
+          type: "GET",
+          data: {
+            "$$app_token" : "jdmsP3hiF9ZYMZMlJTaiBOKly"
+          }
+      }).done(function(data) {
+        apdDataBase = data;
+        console.log('Success! Receive '+data.length+' records!');
+
+        callback(true);
+      }).fail(function() {
+
+        callback(false);
+      });
+    }
+
+  //
+  // Function: Get data
+  //
+    function getDataByCategory (category, limit) {
+
+      var incidentArray = [];  // to be returned to caller
+      var dbLength = apdDataBase.length;
+      limit = limit || dbLength;
+
+      var crimes = getCrimeTypes(category);
+      console.log(limit);
+
+      if(crimes) {
+
+        for(var i = dbLength-1; i > 0; i--) {
+
+          if(crimes.indexOf(apdDataBase[i].crime_type) !== -1) {
+            incidentArray.push(apdDataBase[i]);
+
+            if(incidentArray.length === limit) {
+              break;
+            }
+          }
         }
-    }).done(function(data) {     
-      apdDataBase = data;
-    }); 
-  }
 
-//
-// Function: Get data
-//
-  function getData (incidentList, incidentMax) {   
+        return incidentArray;
 
-    var incidentArray = new Array();  // to be returned to caller
- 
-    apdIndex = apdDataBase.length;
-    incidentCounter =0;
-    iIndex = 0;
+      } else {
 
+        return false;
 
-    while(apdIndex > 0) {  // search the apd database starting at the end
-      apdIndex--;
-      
-      iIndex = 0;     
-      while (iIndex < incidentList.length) {  // compare the apd record to the list of crimes we are looking for
-        if (apdDataBase[apdIndex].crime_type.includes(incidentList[iIndex])) {
-          incidentArray[incidentCounter]=new Array();  // GOT A MATCH!  Log this incident to the array to be returned
-          incidentArray[incidentCounter].crime_type = apdDataBase[apdIndex].crime_type;
-          incidentArray[incidentCounter].address = apdDataBase[apdIndex].address;
-          incidentArray[incidentCounter].date = apdDataBase[apdIndex].date;
-          incidentCounter++;
-          iIndex = incidentList.length;  //break out of this inner while loop because we got a match
-          } // end of if
-        iIndex++;
-        } //end of inner while
+      }
+    }
 
-        if (incidentCounter == incidentMax) {  //if the return array is full, break out of the outer while
-         apdIndex = -1;
+  function getDataByKeyword (keyword, limit) {
+
+      var incidentArray = [];  // to be returned to caller
+      var dbLength = apdDataBase.length;
+      limit = limit || dbLength;
+
+      console.log(limit);
+
+      for(var i = dbLength-1; i > 0; i--) {
+
+        if(apdDataBase[i].crime_type.indexOf(keyword) !== -1) {
+          incidentArray.push(apdDataBase[i]);
+
+          if(incidentArray.length === limit) {
+                break;
+             }  
+          }
         }
-      
-    } //end of outer while
-    return (incidentArray);
-  } // end of function
+          return incidentArray;
+      }
+  
 
 
- 
+
+    function getCrimeTypes(category) {
+
+      var list = getCategoryList();
+      if(list.hasOwnProperty(category)) {
+        return list[category];
+      } else {
+        return false;
+      }
+
+    }
+
+    function getCategoryList() {
+
+      return {
+        violent: ["ASSAULT", "ASLT", "FORCED SODOMY", "KIDNAPPING", "RAPE", "MANSLAUGHTER", "MURDER"],
+        property: ["BURGLARY", "BURG", "ROBBERY"],
+        theft: ["THEFT"],
+        accident: ["CRASH"],
+        drugs: ["DRUG", "CONTROLLED", "MARIJUANA"]
+      };
+    }
+
+    initDatabase(function(flag) {
+      console.log(flag);
+      var mySearchResults = getDataByKeyword ("KIDNAPPING", 3); // search for first 20 violent crimes
+    });
+
+  });
+})(jQuery);
